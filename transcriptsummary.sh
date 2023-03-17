ca9ddf656dbf8e97a532ad615660bc42c65e5476
David Coronel <davecoronel@gmail.com>
2023-03-17 18:03:07 -0400

    takes a youtube id now

--- transcriptsummary.sh
+++ transcriptsummary.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
 
-set -euo pipefail # enable strict error handling
+set -euo pipefail
 
-readonly FILE="transcript.txt"
-readonly OUTPUT_FILE="chatgpt-output.txt"
-readonly NO_TIMESTAMP_FILE="transcript-no-timestamps.txt"
-readonly CHUNK_SIZE=12000
-
-if [ ! -f "$FILE" ]; then
-    echo "Error: File $FILE not found!" >&2
-    exit 1
+# Check if YouTube ID argument is provided
+if [[ $# -eq 0 ]]; then
+  echo "Error: You need to provide the YouTube ID."
+  exit 1
 fi
 
-# remove output file if it exists
-if [ -f "$OUTPUT_FILE" ]; then
-    rm -f "$OUTPUT_FILE"
-fi
+# Set variables
+YOUTUBEID="$1"
+FILE="/tmp/transcript.txt"
+OUTPUT_FILE="/tmp/chatgpt-output.txt"
+CHUNK_SIZE=12000
 
-# remove temporary files if they exist
-trap 'rm -f "$NO_TIMESTAMP_FILE" x??' EXIT
+# Clean up previous output and temporary files
+rm -f "$OUTPUT_FILE"
+rm -f /tmp/x??
 
-# remove timestamp lines and split into chunks
-grep -vE '^[0-9]' "$FILE" > "$NO_TIMESTAMP_FILE"
-split -b "$CHUNK_SIZE" "$NO_TIMESTAMP_FILE"
+# Download transcript and split into chunks
+youtube_transcript_api --format text "$YOUTUBEID" > "$FILE"
+cd /tmp
+split -b "$CHUNK_SIZE" "$FILE"
 
-# process each chunk with ChatGPT and append to output file
-for i in x??
+# Process each chunk with ChatGPT and append to output file
+for file in /tmp/x??
 do
-    chatgpt "give me a comprehensive summary including any important details of the following text: $(cat "$i")" >> "$OUTPUT_FILE"
+    chatgpt "give me a comprehensive summary including any important details of the following transcript: $(cat "$file")" >> "$OUTPUT_FILE"
 done
 
-# open output file in TextEdit (Mac-specific)
+# Clean up temporary files
+rm -f /tmp/x??
+
+# Open output file in TextEdit (Mac-specific)
 if command -v open >/dev/null; then
     open -a TextEdit "$OUTPUT_FILE"
 fi
